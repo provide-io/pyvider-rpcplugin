1cccd353d7bb16df34e87d6937fc339bd3b59ab9
Tim <code@tim.life>
2025-02-23 18:37:14 -0800

    tests/kv/test_kv_integration.py:52: [31m-    transport_name, transport, endpoint = mock_server_transport[m tests/kv/test_kv_integration.py:52: [32m+[m[32m    transport = mock_server_transport[m
    Former-commit-id: af778dc23d0e0073e916021b5ac8414e4e55fade

--- tests/kv/test_kv_integration.py
+++ tests/kv/test_kv_integration.py
@@ -49,7 +49,7 @@ async def kv_server(
     mock_server_transport,
 ):
 
-    transport_name, transport, endpoint = mock_server_transport
+    transport = mock_server_transport
 
     server = RPCPluginServer(
         protocol=KVProtocol(),
