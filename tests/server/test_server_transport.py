e308316caff90b3ed4302ab9dc4ac07057755e3f
Tim <code@tim.life>
2025-02-23 17:29:26 -0800

    tests/server/test_server_transport.py:84: [32m+[m[32m        if isinstance(transport, UnixSocketTransport):[m tests/server/test_server_transport.py:85: [32m+[m[32m            await transport.listen()[m
    Former-commit-id: 596571c93359c618406472f1e095c741ed5c6ca4

--- tests/server/test_server_transport.py
+++ tests/server/test_server_transport.py
@@ -81,6 +81,8 @@ async def test_setup_server_unix_no_socket(
     )
 
     with pytest.raises(TransportError, match="Socket file .* not created"):
+        if isinstance(transport, UnixSocketTransport):
+            await transport.listen()
         await server._setup_server("client_cert")
 
 
