6a91cb500b2b7eba272c6123902b1d362aa0d4b5
Tim <code@tim.life>
2025-02-23 17:34:27 -0800

    tests/server/test_server_transport.py:110: [31m-            #await server._setup_server("client_cert")[m tests/server/test_server_transport.py:112: [32m+[m[32m            await server._setup_server("client_cert")[m

--- tests/server/test_server_transport.py
+++ tests/server/test_server_transport.py
@@ -108,8 +108,8 @@ async def test_setup_server_unix_bad_permissions(
         )
 
         with pytest.raises(TransportError, match="has incorrect permissions"):
-            #await server._setup_server("client_cert")
             await transport.listen()
+            await server._setup_server("client_cert")
     finally:
         if os.path.exists(sock_path):
             os.unlink(sock_path)
