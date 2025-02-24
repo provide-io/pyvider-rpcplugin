64178060e214ae28d6cf18fd8de769a165e68a66
Tim <code@tim.life>
2025-02-23 19:18:53 -0800

    tests/server/test_server_lifecycle.py | 4 +++-

--- tests/server/test_server_lifecycle.py
+++ tests/server/test_server_lifecycle.py
@@ -187,7 +187,9 @@ async def test_wait_for_server_ready(
     mock_server_config,
     mock_server_transport,
 ):
-    transport_name, transport, endpoint = mock_server_transport
+    transport_name = mock_server_transport
+
+    endpoint = await transport.listen()
 
     server = RPCPluginServer(
         protocol=mock_server_protocol,
