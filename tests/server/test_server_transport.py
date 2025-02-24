955b9facd0b9c724206a256139db01cf388b0667
Tim <code@tim.life>
2025-02-23 20:12:08 -0800

    tests/server/test_server_transport.py:170: [31m-    await transport.close()[m tests/server/test_server_transport.py:170: [32m+[m[32m    #await transport.close()[m

--- tests/server/test_server_transport.py
+++ tests/server/test_server_transport.py
@@ -167,7 +167,7 @@ async def test_setup_server_tcp_success(
     await transport.listen()
     #await server._setup_server("client_cert")
     await server.serve()
-    await transport.close()
+    #await transport.close()
 
     # TODO: actually check this shit.
 
