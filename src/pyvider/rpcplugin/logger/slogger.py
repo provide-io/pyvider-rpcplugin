3b890719f04da91bc7253068d8381493f248fd40
Tim <code@tim.life>
2025-02-25 12:34:52 -0800

    src/pyvider/rpcplugin/logger/slogger.py:277: [31m-        return cls(name=name, _logger=root_logger, _structlog=structlog_logger, _processors=processors)[m src/pyvider/rpcplugin/logger/slogger.py:277: [32m+[m[32m        return cls(name=name, _structlog=structlog_logger, _processors=processors)[m

--- src/pyvider/rpcplugin/logger/slogger.py
+++ src/pyvider/rpcplugin/logger/slogger.py
@@ -274,7 +274,7 @@ class SLogger:
             # No configuration was done, use default processors
             processors = []
             
-        return cls(name=name, _logger=root_logger, _structlog=structlog_logger, _processors=processors)
+        return cls(name=name, _structlog=structlog_logger, _processors=processors)
         
     def trace(self, level_or_message: Union[int, str], message: Optional[str] = None, **kwargs: Any) -> None:
         """
