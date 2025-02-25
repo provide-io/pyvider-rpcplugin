360de812d19c0f0d86c507cdb706efa2541a9963
Tim <code@tim.life>
2025-02-25 12:13:39 -0800

    src/pyvider/rpcplugin/logger/slogger.py | 43 +++++++++++++++++++++------------
    Former-commit-id: ddb79504dc6dd1192052e588b04cfc8940f0f809

--- src/pyvider/rpcplugin/logger/slogger.py
+++ src/pyvider/rpcplugin/logger/slogger.py
@@ -1,20 +1,12 @@
 import logging
 import structlog
 
-# Define a custom TRACE level (lower than DEBUG)
-TRACE_LEVEL = 5
-logging.addLevelName(TRACE_LEVEL, "TRACE")
+# Default trace level (lowest possible)
+DEFAULT_TRACE_LEVEL = 33
 
-# Extend logger to support `trace` method
-def trace(self, level, message = None, *args, **kwargs):
-    if self.isEnabledFor(TRACE_LEVEL):
-        self._log(TRACE_LEVEL, message, args, **kwargs)
-
-logging.Logger.trace = trace
-
-# Configure structlog with the new TRACE level
+# Configure structlog with NOTSET to allow custom levels
 structlog.configure(
-    wrapper_class=structlog.make_filtering_bound_logger(TRACE_LEVEL),
+    wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
     processors=[
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.JSONRenderer(),
@@ -22,10 +14,29 @@ structlog.configure(
     logger_factory=structlog.stdlib.LoggerFactory(),
 )
 
+# Extend logger to support `trace` method
+def trace(self, level, message, *args, **kwargs):
+    """
+    Custom trace method that accepts a level and logs with a dynamic log level name.
+    Example: trace(432, "Custom trace message") -> Log level "TRACE432"
+    """
+    if not isinstance(level, int):
+        raise ValueError("Trace level must be an integer.")
+
+    log_level_name = f"TRACE{level}"  # Example: TRACE432
+    logging.addLevelName(level, log_level_name)  # Dynamically register level
+
+    if self.isEnabledFor(level):
+        self._log(level, message, args, **kwargs)
+
+# Attach custom trace method to Logger class
+logging.Logger.trace = trace
+
 # Get a structlog logger
 logger = structlog.get_logger()
 
-# Log messages with the new TRACE level
-logger.trace(111, "This is a trace log")
-logger.debug("This is a debug log")
-logger.info("This is an info log")
+# Example usage
+logger.trace(432, "This is a trace log with level 432")  # Logs as "TRACE432"
+logger.trace(100, "This is a trace log with level 100")  # Logs as "TRACE100"
+logger.debug("This is a debug log")  # Logs as "DEBUG"
+logger.info("This is an info log")  # Logs as "INFO"
