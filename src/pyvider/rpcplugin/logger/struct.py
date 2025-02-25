35303db5c78f6e75f196e16d55f3e6747e50233d
Tim <code@tim.life>
2025-02-25 12:07:19 -0800

    New files added: ?? src/pyvider/rpcplugin/logger/struct.py
    Former-commit-id: 4eb2d2e2cf8294393a0db1851b87857ff6e88bd0

---
+++ src/pyvider/rpcplugin/logger/struct.py
@@ -0,0 +1,31 @@
+import logging
+import structlog
+
+# Define a custom TRACE level (lower than DEBUG)
+TRACE_LEVEL = 5
+logging.addLevelName(TRACE_LEVEL, "TRACE")
+
+# Extend logger to support `trace` method
+def trace(self, level: num, message, *args, **kwargs):
+    if self.isEnabledFor(TRACE_LEVEL):
+        self._log(TRACE_LEVEL, message, args, **kwargs)
+
+logging.Logger.trace = trace
+
+# Configure structlog with the new TRACE level
+structlog.configure(
+    wrapper_class=structlog.make_filtering_bound_logger(TRACE_LEVEL),
+    processors=[
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.JSONRenderer(),
+    ],
+    logger_factory=structlog.stdlib.LoggerFactory(),
+)
+
+# Get a structlog logger
+logger = structlog.get_logger()
+
+# Log messages with the new TRACE level
+logger.trace("This is a trace log")
+logger.debug("This is a debug log")
+logger.info("This is an info log")
