15f6163406b805a0ded2e0c283681080cfd97136
Tim <code@tim.life>
2025-02-25 12:16:29 -0800

    src/pyvider/rpcplugin/logger/slogger.py | 47 +++++++++++++++++----------------
    Former-commit-id: 705aafcf8ee925c1253094d38f4f479e783cc560

--- src/pyvider/rpcplugin/logger/slogger.py
+++ src/pyvider/rpcplugin/logger/slogger.py
@@ -1,27 +1,16 @@
-
-# slogger.py
-
 import logging
 import structlog
 
-# Default trace level (lowest possible)
-DEFAULT_TRACE_LEVEL = 33
+# slogger.py
 
-# Configure structlog with NOTSET to allow custom levels
-structlog.configure(
-    wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
-    processors=[
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.processors.JSONRenderer(),
-    ],
-    logger_factory=structlog.stdlib.LoggerFactory(),
-)
+# Configure default TRACE level (should be lower than DEBUG)
+DEFAULT_TRACE_LEVEL = 5  
 
 # Extend logger to support `trace` method
 def trace(self, level, message, *args, **kwargs):
     """
-    Custom trace method that accepts a level and logs with a dynamic log level name.
-    Example: trace(432, "Custom trace message") -> Log level "TRACE432"
+    Custom trace method that allows dynamic log levels.
+    Example: logger.trace(432, "Message") -> Logs with level TRACE432.
     """
     if not isinstance(level, int):
         raise ValueError("Trace level must be an integer.")
@@ -32,14 +21,26 @@ def trace(self, level, message, *args, **kwargs):
     if self.isEnabledFor(level):
         self._log(level, message, args, **kwargs)
 
-# Attach custom trace method to Logger class
+# Attach the method to `logging.Logger`
 logging.Logger.trace = trace
 
-# Get a structlog logger
-logger = structlog.get_logger()
+# Configure structlog to use standard logging with filtering
+structlog.configure(
+    processors=[
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.JSONRenderer(),
+    ],
+    wrapper_class=structlog.stdlib.BoundLogger,
+    logger_factory=structlog.stdlib.LoggerFactory(),
+)
+
+# Create a standard Python logger and wrap it with structlog
+std_logger = logging.getLogger("slogger")
+std_logger.setLevel(logging.NOTSET)  # Allow all levels
+struct_logger = structlog.wrap_logger(std_logger)
 
 # Example usage
-logger.trace(432, "This is a trace log with level 432")  # Logs as "TRACE432"
-logger.trace(100, "This is a trace log with level 100")  # Logs as "TRACE100"
-logger.debug("This is a debug log")  # Logs as "DEBUG"
-logger.info("This is an info log")  # Logs as "INFO"
+struct_logger.trace(432, "This is a trace log with level 432")  # Logs as "TRACE432"
+struct_logger.trace(100, "This is a trace log with level 100")  # Logs as "TRACE100"
+struct_logger.debug("This is a debug log")  # Logs as "DEBUG"
+struct_logger.info("This is an info log")  # Logs as "INFO"
