1c57499d47778670095fe0b0d6b5ed5a683e7e71
Tim <code@tim.life>
2025-02-27 21:44:21 -0800

    tests/server/test_server_transport.py | 5 ++---
    Former-commit-id: d0ed4d2651919b456e6a95c1fa1ad16062d93c7a

--- tests/server/test_server_transport.py
+++ tests/server/test_server_transport.py
@@ -71,9 +71,8 @@ async def test_setup_server_unix_success_secure(
     )
 
     try:
-        #await transport.listen()
-        await server.serve()
-        # await server._setup_server("client_cert")
+        await transport.listen()
+        await server._setup_server("client_cert")
         # expected = f"unix:{endpoint}"
         #assert any(expected in port for port in dummy_server.ports)
     finally:
