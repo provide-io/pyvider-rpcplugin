b1016872bb194fa3afa429d4ccfdbc9f450cf8f2
Tim <code@tim.life>
2025-02-27 21:42:26 -0800

    tests/server/test_server_transport.py | 7 ++++---
    Former-commit-id: 1375047165e2893b549650b1baa8af707481c8d5

--- tests/server/test_server_transport.py
+++ tests/server/test_server_transport.py
@@ -59,7 +59,8 @@ async def test_setup_server_unix_success_1(
     mock_server_handler,
     mock_server_config,
 ):
-    transport = UnixSocketTransport()
+    sock_path = unique_socket_path
+    transport = UnixSocketTransport(path=sock_path)
 
     server = RPCPluginServer(
         protocol=mock_server_protocol,
@@ -69,13 +70,13 @@ async def test_setup_server_unix_success_1(
     )
 
     try:
-        #await transport.listen()
+        await transport.listen()
         await server.serve()
         # await server._setup_server("client_cert")
         expected = f"unix:{endpoint}"
         #assert any(expected in port for port in dummy_server.ports)
     finally:
-        #await transport.close()
+        await transport.close()
         await server.stop()
 
 @pytest.mark.asyncio
