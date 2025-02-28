f85d040d4cdf99bcf64536010fb1adae50cad6fe
Tim <code@tim.life>
2025-02-27 20:29:01 -0800

    tests/server/test_server_transport.py | 49 +++++++++++++++++------------------
    Former-commit-id: 3091a042eb25bc1319d377d2d8479654f46caeb6

--- tests/server/test_server_transport.py
+++ tests/server/test_server_transport.py
@@ -27,7 +27,7 @@ from tests.conftest import (
 from tests.fixtures import *
 
 @pytest.mark.asyncio
-async def test_setup_server_unix_success(
+async def test_setup_server_unix_success_2(
     tmp_path,
     mock_server_preotocol,
     mock_server_handler,
@@ -59,6 +59,29 @@ async def test_setup_server_unix_success(
         await transport.close()
         if os.path.exists(sock_path):
             os.unlink(sock_path)
+
+@pytest.mark.asyncio
+async def test_setup_server_unix_success_1(
+    tmp_path,
+    mock_server_protocol,
+    mock_server_handler,
+    mock_server_config,
+):
+    transport = UnixSocketTransport()
+
+    server = RPCPluginServer(
+        protocol=mock_server_protocol,
+        handler=mock_server_handler,
+        config=mock_server_config,
+        transport=transport,
+    )
+
+    await transport.listen()
+    await server.serve()
+    # await server._setup_server("client_cert")
+    expected = f"unix:{endpoint}"
+    #assert any(expected in port for port in dummy_server.ports)
+    await transport.close()
 
 @pytest.mark.asyncio
 async def test_setup_server_unix_no_socket_2(
@@ -241,30 +264,6 @@ async def test_setup_server_tcp_success(
     #    for port in server.ports
     #)
 
-
-@pytest.mark.asyncio
-async def test_setup_server_unix_success(
-    tmp_path,
-    mock_server_protocol,
-    mock_server_handler,
-    mock_server_config,
-):
-    transport = UnixSocketTransport()
-
-    server = RPCPluginServer(
-        protocol=mock_server_protocol,
-        handler=mock_server_handler,
-        config=mock_server_config,
-        transport=transport,
-    )
-
-    await transport.listen()
-    await server.serve()
-#    await server._setup_server("client_cert")
-
-    expected = f"unix:{endpoint}"
-    #assert any(expected in port for port in dummy_server.ports)
-    await transport.close()
 
 
 ################################################################################
