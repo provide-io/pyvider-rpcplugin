cd73f948d3eb2e7777bcf70aba16bbe0b9cd6b07
Tim <code@tim.life>
2025-02-27 18:08:39 -0800

    tests/server/test_server_handshake.py:151: [31m-    monkeypatch[m tests/server/test_server_handshake.py:151: [32m+[m[32m    monkeypatch,[m
    Former-commit-id: fde9b808e7fa958e8fc241a5829557d9cca84f63

--- tests/server/test_server_handshake.py
+++ tests/server/test_server_handshake.py
@@ -148,7 +148,7 @@ async def test_negotiate_handshake_via_negotiation(
 # -----------------------------------------------------------------------------
 @pytest.mark.asyncio
 async def test_negotiate_handshake_provided_transport(
-    monkeypatch
+    monkeypatch,
     mock_server_protocol,
     mock_server_handler,
     mock_server_config,
