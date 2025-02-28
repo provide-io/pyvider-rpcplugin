d8481ed5b67ee7f65ad2036f58b155b17df9ba09
Tim <code@tim.life>
2025-02-27 21:44:03 -0800

    tests/server/test_server_transport.py | 6 +++---
    Former-commit-id: c0cee7d5e11a1fa0e583cf79c10f3fb4611c3f8d

--- tests/server/test_server_transport.py
+++ tests/server/test_server_transport.py
@@ -52,7 +52,7 @@ async def test_setup_server_unix_success_insecure(
         await server.stop()
 
 @pytest.mark.asyncio
-async def test_setup_server_unix_success_1(
+async def test_setup_server_unix_success_secure(
     tmp_path,
     client_cert,
     unique_socket_path,
@@ -71,10 +71,10 @@ async def test_setup_server_unix_success_1(
     )
 
     try:
-        await transport.listen()
+        #await transport.listen()
         await server.serve()
         # await server._setup_server("client_cert")
-        expected = f"unix:{endpoint}"
+        # expected = f"unix:{endpoint}"
         #assert any(expected in port for port in dummy_server.ports)
     finally:
         await transport.close()
