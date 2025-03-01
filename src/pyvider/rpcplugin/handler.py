971a95890f316ef725fd29741369ce314db55fc2
Tim <code@tim.life>
2025-03-01 14:27:06 -0800

    result.json                      | 1 +  src/pyvider/rpcplugin/handler.py | 7 ++-----
    Former-commit-id: d773af92591e489e6693e5d1c24c32a86ebaa079 [formerly e406fb93cdee760e0abb91901ee56ccaaa810c10]
    Former-commit-id: 40b03214e900a1388511d2a03538e4133ee9477e

--- src/pyvider/rpcplugin/handler.py
+++ src/pyvider/rpcplugin/handler.py
@@ -1,13 +1,10 @@
 
 # pyvider/rpcplugin/handler.py
 
-# pragma: no cover
-
-from typing import Protocol, runtime_checkable
-
+from typing import Protocol, runtime_checkable               # pragma: no cover
 
 @runtime_checkable
-class RPCPluginHandler(Protocol): # pragma: no cover
+class RPCPluginHandler(Protocol):                            # pragma: no cover
     """
     Base protocol that all RPC handlers must implement.
     The actual methods required will be defined by the specific gRPC service.
