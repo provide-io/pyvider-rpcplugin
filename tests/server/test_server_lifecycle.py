d0bfc48dad14dc4ba3f063473169183da64167da
Tim <code@tim.life>
2025-03-01 09:47:00 -0800

    tests/server/test_server_lifecycle.py:32: [31m-@pytest.mark.skip[m tests/server/test_server_lifecycle.py:32: [32m+[m[32m@pytest.mark.asyncio[m
    Former-commit-id: 93143cf621f341a52e2ed6589a9d9726d9eb3e04

--- tests/server/test_server_lifecycle.py
+++ tests/server/test_server_lifecycle.py
@@ -29,7 +29,7 @@ from tests.conftest import (
 
 from tests.fixtures import *
 
-@pytest.mark.skip
+@pytest.mark.asyncio
 async def test_server_serve_runtime_error(
     monkeypatch,
     mock_server_handler,
