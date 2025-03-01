7723f7332a5af15126e49b452e48a70665280f62
Tim <code@tim.life>
2025-03-01 09:44:59 -0800

    tests/server/test_server_transport.py:54: [31m-@pytest.mark.skip[m tests/server/test_server_transport.py:54: [32m+[m[32m@pytest.mark.asyncio[m
    Former-commit-id: eb79c9d66da2535fb7776657988e3a7e3cdf2d2d

--- tests/server/test_server_transport.py
+++ tests/server/test_server_transport.py
@@ -51,7 +51,7 @@ async def test_setup_server_unix_success_insecure(
     finally:
         await server.stop()
 
-@pytest.mark.skip
+@pytest.mark.asyncio
 async def test_setup_server_unix_success_secure(
     tmp_path,
     client_cert,
