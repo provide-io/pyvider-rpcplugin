d6c7429ddbbc4d0a8cf1ade29411c6a4dd9cfcab
Tim <code@tim.life>
2025-09-13 19:21:18 -0700

    🔼⚙️ [skip ci] auto-commit

--- examples/ch02_quick_start_client.py
+++ examples/ch02_quick_start_client.py
@@ -14,7 +14,7 @@ import example_utils  # type: ignore[import-not-found] # noqa: E402
 example_utils.configure_for_example(clear_env=True)  # Client context
 
 from pyvider.rpcplugin import plugin_client  # noqa: E402
-from pyvider.rpcplugin.client.base import (  # noqa: E402
+from pyvider.rpcplugin.client import (  # noqa: E402
     RPCPluginClient,
 )  # Retaining for clarity if user inspects client object
 from pyvider.rpcplugin.exception import RPCPluginError  # noqa: E402
