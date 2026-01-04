145807e5620e12c73f63df08219ba1250e3c084e
Tim <code@tim.life>
2026-01-04 15:19:36 -0800

    🔼⚙️ [skip ci] auto-commit
    (cherry picked from commit 8da0b8439e154e73fc83bcb7c2a9f3592f2cd405)

--- examples/security_mtls_example.py
+++ examples/security_mtls_example.py
@@ -3,13 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-"""TODO: Add module docstring."""
-
-#!/usr/bin/env python3
-# SPDX-FileCopyrightText: Copyright (c) 2025 provide.io llc. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-
 """Security and mTLS Configuration - Functional Example.
 This example demonstrates a working mTLS setup between a client and a server
 plugin launched as a subprocess."""
